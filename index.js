8d3e63928c38a03aa5669acfcf7daa261e4e4e6f
Evan <evangulyas@outlook.com>
2015-09-16 19:18:58 -0400

    add normals

--- index.js
+++ index.js
@@ -3,19 +3,22 @@ var gl;
 
 var somethingVertexPositionBuffer;
 var somethingVertexColorBuffer;
+var somethingVertexNormalBuffer;
 var somethingVertexIndexBuffer;
 
 var shaderProgram;
 
 var mvMatrix = mat4.create();
 var pMatrix = mat4.create();
+var nMatrix = mat3.create();
 
 var vertices = [];
 var colors = [];
+var normals = [];
 var indices = [];
 
-var numBlocks = 5;
-var secondNumBlocks = 30;
+var numBlocks = 10;
+var secondNumBlocks = 50;
 
 var moving = [];
 function generateMoving() {
@@ -55,60 +58,43 @@ function generateVertices() {
   var currentY = 1;
   var currentIndex = 0;
 
-  var changeSomething = false;
-  var changeColor = false;
+  var changeSide = false;
 
   for (var i = 0; i < numBlocks; i++) {
 
     for (var l = 0; l < secondNumBlocks; l++) {
 
-      var tempVertices = [];
-
-      if (!changeSomething) {
-        tempVertices = [
+      if (!changeSide) {
+        vertices = vertices.concat([
           currentX, currentY, 0.0,
           currentX + 2, currentY - 2, 0.0,
           currentX, currentY - 2, 0.0
-        ]
+        ]);
       }
       else {
-        tempVertices = [
+        vertices = vertices.concat([
           currentX, currentY, 0.0,
           currentX + 2, currentY, 0.0,
           currentX + 2, currentY - 2, 0.0
-        ]
+        ]);
       }
 
-      var tempColors = [];
-      var somethingColor = [];
-
-      if (!changeColor) {
-        somethingColor = [0.0, 0.0, 0.0, 1.0];
-        changeColor = true;
-      }
-      else {
-        somethingColor = [0.5, 0.5, 0.5, 1.0];
-        changeColor = false;
+      for (var j = 0; j < 3 * 4; j++) {
+        colors = colors.concat([0.1, 0.1, 0.1, 1]);
       }
 
-      while (tempColors.length < 3 * 4) {
-        tempColors = tempColors.concat(somethingColor);
+      for (var j = 0; j < 3; j++) {
+        normals = normals.concat(0, 0, -1);
       }
 
-      var tempIndices = [
-        currentIndex, currentIndex + 1, currentIndex + 2
-      ];
+      indices = indices.concat(currentIndex, currentIndex + 1, currentIndex + 2);
 
-      vertices = vertices.concat(tempVertices);
-      colors = colors.concat(tempColors);
-      indices = indices.concat(tempIndices);
-
-      if (!changeSomething) {
-        changeSomething = true;
+      if (!changeSide) {
+        changeSide = true;
       }
       else {
         currentX += 2;
-        changeSomething = false;
+        changeSide = false;
       }
 
       currentIndex += 3;
@@ -125,10 +111,10 @@ function generateVertices() {
 function startMove() {
   setInterval(function () {
     move();
-  }, 100);
+  }, 25);
   setInterval(function () {
     setMove();
-  }, 100);
+  }, 10);
 }
 
 function setMove() {
@@ -308,46 +294,76 @@ function setMove() {
 function move() {
 
   for (var i = 0; i < moving.length; i++) {
+
     for (var l = 0; l < moving[i].length; l++) {
+
       ['one', 'two', 'three'].forEach(function (e) {
 
-        var block = moving[i][l][e];
+        var point = moving[i][l][e];
+        var indice = (l * (3 * 3)) + (i * (secondNumBlocks * (3 * 3)));
 
-        if (block.isMoving) {
+        if (point.isMoving) {
 
-          if (block.isMovingUp) {
-            block.current = block.current + 0.1;
-            if (block.current > 1) {
-              block.isMovingUp = false;
+          if (point.isMovingUp) {
+            point.current = point.current + 0.01;
+            if (point.current > 1.0) {
+              point.isMovingUp = false;
             }
           }
           else {
-            block.current = block.current - 0.1;
-            if (block.current < 0) {
-              block.current = 0;
-              block.isMoving = false;
+            point.current = point.current - 0.01;
+            if (point.current < 0) {
+              point.current = 0;
+              point.isMoving = false;
             }
           }
 
-          var temp = (l * (3 * 3)) + (i * (secondNumBlocks * (3 * 3)))
-          var indice = 0;
+          if (e === 'one') vertices[indice + 2] = point.current;
+          if (e === 'two') vertices[indice + 5] = point.current;
+          if (e === 'three') vertices[indice + 8] = point.current;
 
-          if (e === 'one') {
-            indice = temp + 2;
+          var pointOne = [vertices[indice], vertices[indice + 1], vertices[indice + 2]];
+          var pointTwo = [vertices[indice + 3], vertices[indice + 4], vertices[indice + 5]];
+          var pointThree = [vertices[indice + 6], vertices[indice + 7], vertices[indice + 8]];
+
+          var vectorOne = {
+            x: pointTwo[0] - pointOne[0],
+            y: pointTwo[1] - pointOne[1],
+            z: pointTwo[2] - pointOne[2]
           }
-          if (e === 'two') {
-            indice = temp + 5;
-          }
-          if (e === 'three') {
-            indice = temp + 8;
+          var vectorTwo = {
+            x: pointThree[0] - pointOne[0],
+            y: pointThree[1] - pointOne[1],
+            z: pointThree[2] - pointOne[2]
           }
 
-          vertices[indice] = block.current;
+          var normalX = (vectorOne.y * vectorTwo.z) - (vectorOne.z * vectorTwo.y);
+          var normalY = (vectorOne.z * vectorTwo.x) - (vectorOne.x * vectorTwo.z);
+          var normalZ = (vectorOne.x * vectorTwo.y) - (vectorOne.y * vectorTwo.x);
+
+          if (normalX > 1) normalX = 1;
+          if (normalX < -1) normalX = -1;
+          if (normalY > 1) normalY = 1;
+          if (normalY < -1) normalY = -1;
+          if (normalZ > 1) normalZ = 1;
+          if (normalZ < -1) normalZ = -1;
+
+          normals[indice] = normalX;
+          normals[indice + 1] = normalY;
+          normals[indice + 2] = normalZ
+          normals[indice + 3] = normalX;
+          normals[indice + 4] = normalY;
+          normals[indice + 5] = normalZ
+          normals[indice + 6] = normalX;
+          normals[indice + 7] = normalY;
+          normals[indice + 8] = normalZ;
 
         }
 
       });
+
     }
+
   }
 
   initBuffers();
@@ -356,8 +372,14 @@ function move() {
 }
 
 function setMatrixUniforms() {
+
   gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, pMatrix);
   gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
+
+  mat4.toInverseMat3(mvMatrix, nMatrix);
+  mat3.transpose(nMatrix);
+  gl.uniformMatrix3fv(shaderProgram.nMatrixUniform, false, nMatrix);
+
 }
 
 function initGL(canvas) {
@@ -435,8 +457,12 @@ function initShaders() {
   shaderProgram.vertexColorAttribute = gl.getAttribLocation(shaderProgram, 'aVertexColor');
   gl.enableVertexAttribArray(shaderProgram.vertexColorAttribute);
 
+  shaderProgram.vertexNormalAttribute = gl.getAttribLocation(shaderProgram, 'aVertexNormal');
+  gl.enableVertexAttribArray(shaderProgram.vertexNormalAttribute);
+
   shaderProgram.pMatrixUniform = gl.getUniformLocation(shaderProgram, 'uPMatrix');
   shaderProgram.mvMatrixUniform = gl.getUniformLocation(shaderProgram, 'uMVMatrix');
+  shaderProgram.nMatrixUniform = gl.getUniformLocation(shaderProgram, 'uNMatrix');
 
 }
 
@@ -453,6 +479,12 @@ function initBuffers() {
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
   somethingVertexColorBuffer.itemSize = 4;
   somethingVertexColorBuffer.numItems = 3 * (numBlocks * secondNumBlocks);
+
+  somethingVertexNormalBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, somethingVertexNormalBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normals), gl.STATIC_DRAW);
+  somethingVertexNormalBuffer.itemSize = 3;
+  somethingVertexNormalBuffer.numItems = 3 * (numBlocks * secondNumBlocks);
 
   somethingVertexIndexBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, somethingVertexIndexBuffer);
@@ -478,6 +510,9 @@ function drawScene() {
 
   gl.bindBuffer(gl.ARRAY_BUFFER, somethingVertexColorBuffer);
   gl.vertexAttribPointer(shaderProgram.vertexColorAttribute, somethingVertexColorBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, somethingVertexNormalBuffer);
+  gl.vertexAttribPointer(shaderProgram.vertexNormalAttribute, somethingVertexNormalBuffer.itemSize, gl.FLOAT, false, 0, 0);
 
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, somethingVertexIndexBuffer);
   setMatrixUniforms();
