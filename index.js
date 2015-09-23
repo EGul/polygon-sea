abbff15a253194a80434331a8f3d1ead3611e9b9
Evan <evangulyas@outlook.com>
2015-09-23 01:51:24 -0400

    Refactor moving

--- index.js
+++ index.js
@@ -40,6 +40,8 @@ function PolygonSea(opts) {
   var indices = [];
 
   var moving = [];
+  var isMoving = [];
+  var isNotMoving = [];
 
   this.setup = setup;
   this.setMove = setMove;
@@ -49,13 +51,13 @@ function PolygonSea(opts) {
   function generateMoving() {
     var isLeft = true;
     for (var i = 0; i < opts.xBlocks; i++) {
-      var somethingMove = [];
+      var points = [];
       for (var l = 0; l < opts.yBlocks; l++) {
-        somethingMove.push({
+        points.push({
           isLeft: isLeft,
-          one: {isMoving: false, isMovingUp: false, current: 0},
-          two: {isMoving: false, isMovingUp: false, current: 0},
-          three: {isMoving: false, isMovingUp: false, current: 0}
+          one: {index: 0, xIndex: l, yIndex: i, isLeft: isLeft, isMoving: false, isMovingUp: false, current: 0},
+          two: {index: 1, xIndex: l, yIndex: i, isLeft: isLeft, isMoving: false, isMovingUp: false, current: 0},
+          three: {index: 2, xIndex: l, yIndex: i, isLeft: isLeft, isMoving: false, isMovingUp: false, current: 0},
         })
         if (!isLeft) {
           isLeft = true;
@@ -64,7 +66,12 @@ function PolygonSea(opts) {
           isLeft = false;
         }
       }
-      moving.push(somethingMove);
+      moving.push(points);
+      for (var l = 0; l < points.length; l++) {
+        isNotMoving.push(points[l].one);
+        isNotMoving.push(points[l].two);
+        isNotMoving.push(points[l].three);
+      }
     }
   }
 
@@ -214,173 +221,124 @@ function PolygonSea(opts) {
 
   function setMove() {
 
-    var randomOne = Math.floor(Math.random() * opts.xBlocks);
-    var randomTwo = Math.floor(Math.random() * opts.yBlocks);
-    var randomIndex = Math.floor(Math.random() * 3);
+    if (!isNotMoving.length) {
+      return false;
+    }
 
-    var moveBlock = moving[randomOne][randomTwo];
-    var somethingIsLeft = moveBlock.isLeft;
+    var random = Math.floor(Math.random() * isNotMoving.length);
 
-    var blockOneIndex = 0;
-    var blockOneIndexTwo = 0;
+    var point = isNotMoving[random];
+    var points = [];
+    var index = point.index;
+    var xIndex = point.xIndex;
+    var yIndex = point.yIndex;
 
-    var blockTwoIndex = 0;
-    var blockTwoIndexTwo = 0;
+    if (point.isLeft) {
 
-    var blockThreeIndex = 0;
-    var blockThreeIndexTwo = 0;
-
-    var blockFourIndex = 0;
-    var blockFourIndexTwo = 0;
-
-    if (somethingIsLeft) {
-
-      if (randomIndex === 0) {
-        blockOneIndex = moveBlock.one;
-        if (randomTwo + 1 < opts.yBlocks) {
-          blockOneIndexTwo = moving[randomOne][randomTwo + 1].one;
+      if (index === 0) {
+        points.push(moving[yIndex][xIndex].one);
+        if (xIndex + 1 < opts.yBlocks) {
+          points.push(moving[yIndex][xIndex + 1].one);
         }
-        if (randomTwo - 1 > -1) {
-          blockTwoIndexTwo = moving[randomOne][randomTwo - 1].two;
+        if (xIndex - 1 > -1) {
+          points.push(moving[yIndex][xIndex - 1].two);
         }
-        if ((randomTwo - 1 > -1) && (randomOne + 1 < opts.xBlocks)) {
-          blockThreeIndex = moving[randomOne + 1][randomTwo - 2].two;
-          blockThreeIndexTwo = moving[randomOne + 1][randomTwo - 1].three;
+        if ((xIndex - 1 > -1) && (yIndex + 1 < opts.xBlocks)) {
+          points.push(moving[yIndex + 1][xIndex - 2].two);
+          points.push(moving[yIndex + 1][xIndex - 1].three);
         }
-        if (randomOne + 1 < opts.xBlocks) {
-          blockFourIndex = moving[randomOne + 1][randomTwo].three;
+        if (yIndex + 1 < opts.xBlocks) {
+          points.push(moving[yIndex + 1][xIndex].three);
         }
       }
 
-      if (randomIndex === 1) {
-        blockOneIndex = moveBlock.two;
-        if (randomTwo + 1 < opts.yBlocks) {
-          blockOneIndexTwo = moving[randomOne][randomTwo + 1].three;
+      if (index === 1) {
+        points.push(moving[yIndex][xIndex].two);
+        if (xIndex + 1 < opts.yBlocks) {
+          points.push(moving[yIndex][xIndex + 1].three);
         }
-        if (randomTwo + 2 < opts.yBlocks) {
-          blockTwoIndex = moving[randomOne][randomTwo + 2].three;
+        if (xIndex + 2 < opts.yBlocks) {
+          points.push(moving[yIndex][xIndex + 2].three);
         }
-        if ((randomTwo + 2 < opts.yBlocks) && (randomOne - 1 > -1)) {
-          blockThreeIndex = moving[randomOne - 1][randomTwo + 2].one;
-          blockThreeIndexTwo = moving[randomOne - 1][randomTwo + 3].one;
+        if ((xIndex + 2 < opts.yBlocks) && (yIndex - 1 > -1)) {
+          points.push(moving[yIndex - 1][xIndex + 2].one);
+          points.push(moving[yIndex - 1][xIndex + 3].one);
         }
-        if (randomOne - 1 > -1) {
-          blockFourIndex = moving[randomOne - 1][randomTwo + 1].two;
+        if (yIndex - 1 > -1) {
+          points.push(moving[yIndex - 1][xIndex + 1].two);
         }
       }
 
-      if (randomIndex === 2) {
-        blockOneIndex = moveBlock.three;
-        if (randomTwo - 1 > -1) {
-          blockTwoIndex = moving[randomOne][randomTwo - 1].three;
-          blockTwoIndexTwo = moving[randomOne][randomTwo - 2].two;
+      if (index === 2) {
+        points.push(moving[yIndex][xIndex].three);
+        if (xIndex - 1 > -1) {
+          points.push(moving[yIndex][xIndex - 1].three);
+          points.push(moving[yIndex][xIndex - 2].two);
         }
-        if ((randomTwo - 1 > -1) && (randomOne - 1 > -1)) {
-          blockThreeIndex = moving[randomOne - 1][randomTwo - 1].two;
+        if ((xIndex - 1 > -1) && (yIndex - 1 > -1)) {
+          points.push(moving[yIndex - 1][xIndex - 1].two);
         }
-        if (randomOne - 1 > -1) {
-          blockFourIndex = moving[randomOne - 1][randomTwo].one;
-          blockFourIndexTwo = moving[randomOne - 1][randomTwo + 1].one;
+        if (yIndex - 1 > -1) {
+          points.push(moving[yIndex - 1][xIndex].one);
+          points.push(moving[yIndex - 1][xIndex + 1].one);
         }
       }
 
     }
     else {
 
-      if (randomIndex === 0) {
-        blockOneIndex = moveBlock.one;
-        blockOneIndexTwo = moving[randomOne][randomTwo - 1].one;
-        if (randomTwo - 2 > -1) {
-          blockTwoIndex = moving[randomOne][randomTwo - 2].two;
+      if (index === 0) {
+        points.push(moving[yIndex][xIndex].one);
+        points.push(moving[yIndex][xIndex - 1].one);
+        if (xIndex - 2 > -1) {
+          points.push(moving[yIndex][xIndex - 2].two);
         }
-        if ((randomTwo - 2 > -1) && (randomOne + 1 < opts.xBlocks)) {
-          blockThreeIndex = moving[randomOne + 1][randomTwo - 2].three;
-          blockThreeIndexTwo = moving[randomOne + 1][randomTwo - 3].two;
+        if ((xIndex - 2 > -1) && (yIndex + 1 < opts.xBlocks)) {
+          points.push(moving[yIndex + 1][xIndex - 2].three);
+          points.push(moving[yIndex + 1][xIndex - 3].two);
         }
-        if (randomOne + 1 < opts.xBlocks) {
-          blockFourIndex = moving[randomOne + 1][randomTwo - 1].three;
-        }
-      }
-
-      if (randomIndex === 1) {
-        blockOneIndex = moveBlock.two;
-        if (randomTwo + 1 < opts.yBlocks) {
-          blockTwoIndex = moving[randomOne][randomTwo + 1].one;
-          blockTwoIndexTwo = moving[randomOne][randomTwo + 2].one;
-        }
-        if ((randomTwo + 1 < opts.yBlocks) && (randomOne + 1 < opts.xBlocks)) {
-          blockThreeIndex = moving[randomOne + 1][randomTwo + 1].three;
-        }
-        if (randomOne + 1 < opts.xBlocks) {
-          blockFourIndex = moving[randomOne + 1][randomTwo].three;
-          blockFourIndexTwo = moving[randomOne + 1][randomTwo - 1].two;
+        if (yIndex + 1 < opts.xBlocks) {
+          points.push(moving[yIndex + 1][xIndex - 1].three);
         }
       }
 
-      if (randomIndex === 2) {
-        blockOneIndex = moveBlock.three;
-        blockOneIndexTwo = moving[randomOne][randomTwo - 1].two;
-        if (randomTwo + 1 < opts.yBlocks) {
-          blockTwoIndex = moving[randomOne][randomTwo + 1].three;
+      if (index === 1) {
+        points.push(moving[yIndex][xIndex].two);
+        if (xIndex + 1 < opts.yBlocks) {
+          points.push(moving[yIndex][xIndex + 1].one);
+          points.push(moving[yIndex][xIndex + 2].one);
         }
-        if ((randomTwo + 1 < opts.yBlocks) && (randomOne - 1 > -1)) {
-          blockThreeIndex = moving[randomOne - 1][randomTwo + 1].one;
-          blockThreeIndexTwo = moving[randomOne - 1][randomTwo + 2].one;
+        if ((xIndex + 1 < opts.yBlocks) && (yIndex + 1 < opts.xBlocks)) {
+          points.push(moving[yIndex + 1][xIndex + 1].three);
         }
-        if (randomOne - 1 > -1) {
-          blockFourIndex = moving[randomOne - 1][randomTwo].two;
+        if (yIndex + 1 < opts.xBlocks) {
+          points.push(moving[yIndex + 1][xIndex].three);
+          points.push(moving[yIndex + 1][xIndex - 1].two);
+        }
+      }
+
+      if (index === 2) {
+        points.push(moving[yIndex][xIndex].three);
+        points.push(moving[yIndex][xIndex - 1].two);
+        if (xIndex + 1 < opts.yBlocks) {
+          points.push(moving[yIndex][xIndex + 1].three);
+        }
+        if ((xIndex + 1 < opts.yBlocks) && (yIndex - 1 > -1)) {
+          points.push(moving[yIndex - 1][xIndex + 1].one);
+          points.push(moving[yIndex - 1][xIndex + 2].one);
+        }
+        if (yIndex - 1 > -1) {
+          points.push(moving[yIndex - 1][xIndex].two);
         }
       }
 
     }
 
-    if (!blockOneIndex.isMoving) {
-      blockOneIndex.isMoving = true;
-      blockOneIndex.isMovingUp = true;
-    }
-    if (blockOneIndexTwo) {
-      if (!blockOneIndexTwo.isMoving) {
-        blockOneIndexTwo.isMoving = true;
-        blockOneIndexTwo.isMovingUp = true;
-      }
-    }
-
-    if (blockTwoIndex) {
-      if (!blockTwoIndex.isMoving) {
-        blockTwoIndex.isMoving = true;
-        blockTwoIndex.isMovingUp = true;
-      }
-    }
-    if (blockTwoIndexTwo) {
-      if (!blockTwoIndexTwo.isMoving) {
-        blockTwoIndexTwo.isMoving = true;
-        blockTwoIndexTwo.isMovingUp = true;
-      }
-    }
-
-    if (blockThreeIndex) {
-      if (!blockThreeIndex.isMoving) {
-        blockThreeIndex.isMoving = true;
-        blockThreeIndex.isMovingUp = true;
-      }
-    }
-    if (blockThreeIndexTwo) {
-      if (!blockThreeIndexTwo.isMoving) {
-        blockThreeIndexTwo.isMoving = true;
-        blockThreeIndexTwo.isMovingUp = true;
-      }
-    }
-
-    if (blockFourIndex) {
-      if (!blockFourIndex.isMoving) {
-        blockFourIndex.isMoving = true;
-        blockFourIndex.isMovingUp = true;
-      }
-    }
-    if (blockFourIndexTwo) {
-      if (!blockFourIndexTwo.isMoving) {
-        blockFourIndexTwo.isMoving = true;
-        blockFourIndexTwo.isMovingUp = true;
+    for (var i = 0; i < points.length; i++) {
+      if (!points[i].isMoving) {
+        points[i].isMoving = true;
+        points[i].isMovingUp = true;
+        isMoving.push(isNotMoving.splice(isNotMoving.indexOf(points[i]), 1)[0]);
       }
     }
 
@@ -388,76 +346,65 @@ function PolygonSea(opts) {
 
   function move() {
 
-    for (var i = 0; i < moving.length; i++) {
+    for (var i = isMoving.length - 1; i > -1; i--) {
 
-      for (var l = 0; l < moving[i].length; l++) {
+      var point = isMoving[i];
+      var indice = (point.xIndex * (3 * 3)) + (point.yIndex * (opts.yBlocks * (3 * 3)));
 
-        ['one', 'two', 'three'].forEach(function (e) {
-
-          var point = moving[i][l][e];
-          var indice = (l * (3 * 3)) + (i * (opts.yBlocks * (3 * 3)));
-
-          if (point.isMoving) {
-
-            if (point.isMovingUp) {
-              point.current = point.current + opts.moveIncrement;
-              if (point.current > opts.maxMove) {
-                point.isMovingUp = false;
-              }
-            }
-            else {
-              point.current = point.current - opts.moveIncrement;
-              if (point.current < 0) {
-                point.current = 0;
-                point.isMoving = false;
-              }
-            }
-
-            if (e === 'one') vertices[indice + 2] = point.current;
-            if (e === 'two') vertices[indice + 5] = point.current;
-            if (e === 'three') vertices[indice + 8] = point.current;
-
-            var pointOne = [vertices[indice], vertices[indice + 1], vertices[indice + 2]];
-            var pointTwo = [vertices[indice + 3], vertices[indice + 4], vertices[indice + 5]];
-            var pointThree = [vertices[indice + 6], vertices[indice + 7], vertices[indice + 8]];
-
-            var vectorOne = {
-              x: pointTwo[0] - pointOne[0],
-              y: pointTwo[1] - pointOne[1],
-              z: pointTwo[2] - pointOne[2]
-            }
-            var vectorTwo = {
-              x: pointThree[0] - pointOne[0],
-              y: pointThree[1] - pointOne[1],
-              z: pointThree[2] - pointOne[2]
-            }
-
-            var normalX = (vectorOne.y * vectorTwo.z) - (vectorOne.z * vectorTwo.y);
-            var normalY = (vectorOne.z * vectorTwo.x) - (vectorOne.x * vectorTwo.z);
-            var normalZ = (vectorOne.x * vectorTwo.y) - (vectorOne.y * vectorTwo.x);
-
-            if (normalX > 1) normalX = 1;
-            if (normalX < -1) normalX = -1;
-            if (normalY > 1) normalY = 1;
-            if (normalY < -1) normalY = -1;
-            if (normalZ > 1) normalZ = 1;
-            if (normalZ < -1) normalZ = -1;
-
-            normals[indice] = normalX;
-            normals[indice + 1] = normalY;
-            normals[indice + 2] = normalZ
-            normals[indice + 3] = normalX;
-            normals[indice + 4] = normalY;
-            normals[indice + 5] = normalZ
-            normals[indice + 6] = normalX;
-            normals[indice + 7] = normalY;
-            normals[indice + 8] = normalZ;
-
-          }
-
-        });
-
+      if (point.isMovingUp) {
+        point.current = point.current + opts.moveIncrement;
+        if (point.current > opts.maxMove) {
+          point.isMovingUp = false;
+        }
       }
+      else {
+        point.current = point.current - opts.moveIncrement;
+        if (point.current < 0) {
+          point.current = 0;
+          point.isMoving = false;
+          isNotMoving.push(isMoving.splice(isMoving.indexOf(point), 1)[0]);
+        }
+      }
+
+      if (point.index === 0) vertices[indice + 2] = point.current;
+      if (point.index === 1) vertices[indice + 5] = point.current;
+      if (point.index === 2) vertices[indice + 8] = point.current;
+
+      var pointOne = [vertices[indice], vertices[indice + 1], vertices[indice + 2]];
+      var pointTwo = [vertices[indice + 3], vertices[indice + 4], vertices[indice + 5]];
+      var pointThree = [vertices[indice + 6], vertices[indice + 7], vertices[indice + 8]];
+
+      var vectorOne = {
+        x: pointTwo[0] - pointOne[0],
+        y: pointTwo[1] - pointOne[1],
+        z: pointTwo[2] - pointOne[2]
+      }
+      var vectorTwo = {
+        x: pointThree[0] - pointOne[0],
+        y: pointThree[1] - pointOne[1],
+        z: pointThree[2] - pointOne[2]
+      }
+
+      var normalX = (vectorOne.y * vectorTwo.z) - (vectorOne.z * vectorTwo.y);
+      var normalY = (vectorOne.z * vectorTwo.x) - (vectorOne.x * vectorTwo.z);
+      var normalZ = (vectorOne.x * vectorTwo.y) - (vectorOne.y * vectorTwo.x);
+
+      if (normalX > 1) normalX = 1;
+      if (normalX < -1) normalX = -1;
+      if (normalY > 1) normalY = 1;
+      if (normalY < -1) normalY = -1;
+      if (normalZ > 1) normalZ = 1;
+      if (normalZ < -1) normalZ = -1;
+
+      normals[indice] = normalX;
+      normals[indice + 1] = normalY;
+      normals[indice + 2] = normalZ
+      normals[indice + 3] = normalX;
+      normals[indice + 4] = normalY;
+      normals[indice + 5] = normalZ
+      normals[indice + 6] = normalX;
+      normals[indice + 7] = normalY;
+      normals[indice + 8] = normalZ;
 
     }
 
